2b75b0748faf7153f18b8288f886ece324891bec
AliCedroni66 <alicedroni66@gmail.com>
2016-05-26 01:14:34 -0500

    Moar sounds
    Audio middleware session nearly finalized (only really needs some mix
    tweaks). Ambiences and nurture triggers implemented. Coding needed to
    change states of ambiences and triggers for choices need to be created.

--- Assets/StreamingAssets/Audio/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/Wwise_IDs.h
@@ -13,12 +13,110 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_CAT1SAVE = 2223085068U;
-        static const AkUniqueID PLAY_MUSIC130 = 1541497333U;
+        static const AkUniqueID PLAY_AMB = 2959533290U;
+        static const AkUniqueID PLAY_BIRDCALLS = 1700644942U;
+        static const AkUniqueID PLAY_CATDISTRESS = 1792286211U;
+        static const AkUniqueID PLAY_CATPURRLOUD = 767653747U;
+        static const AkUniqueID PLAY_CATSAVE = 275729277U;
+        static const AkUniqueID PLAY_CHACHINGBIG = 982264143U;
+        static const AkUniqueID PLAY_CHACHINGSMALL = 2489948288U;
+        static const AkUniqueID PLAY_CHILD = 2952268476U;
+        static const AkUniqueID PLAY_CHURCHBELLS = 3419492923U;
+        static const AkUniqueID PLAY_DIVORCE = 1407659604U;
+        static const AkUniqueID PLAY_GUITAR = 3564911592U;
+        static const AkUniqueID PLAY_JAIL = 3222604894U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
         static const AkUniqueID PLAY_PADDLEF = 3452829096U;
         static const AkUniqueID PLAY_PADDLEL = 3452829090U;
         static const AkUniqueID PLAY_PADDLER = 3452829116U;
+        static const AkUniqueID PLAY_PLANT = 2689642121U;
+        static const AkUniqueID PLAY_SCHOOLBELL = 4147217813U;
+        static const AkUniqueID PLAY_WATER = 441572235U;
+        static const AkUniqueID PLAY_WOODCREAK = 3217967095U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace CAT
+        {
+            static const AkUniqueID GROUP = 983016379U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAT1 = 4155948880U;
+                static const AkUniqueID CAT2 = 4155948883U;
+                static const AkUniqueID CAT3 = 4155948882U;
+            } // namespace STATE
+        } // namespace CAT
+
+        namespace MOOD
+        {
+            static const AkUniqueID GROUP = 3128647864U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NEGATIVE = 4219547688U;
+                static const AkUniqueID NEUTRAL = 670611050U;
+                static const AkUniqueID POSITIVE = 1192865152U;
+            } // namespace STATE
+        } // namespace MOOD
+
+        namespace PACE
+        {
+            static const AkUniqueID GROUP = 1895473648U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FAST = 2965380179U;
+                static const AkUniqueID MEDIUM = 2849147824U;
+                static const AkUniqueID SLOW = 787604482U;
+                static const AkUniqueID START = 1281810935U;
+                static const AkUniqueID STOPPED = 2904797076U;
+            } // namespace STATE
+        } // namespace PACE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace CAT
+        {
+            static const AkUniqueID GROUP = 983016379U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CAT1 = 4155948880U;
+                static const AkUniqueID CAT2 = 4155948883U;
+                static const AkUniqueID CAT3 = 4155948882U;
+            } // namespace SWITCH
+        } // namespace CAT
+
+        namespace MOOD
+        {
+            static const AkUniqueID GROUP = 3128647864U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID NEGATIVE = 4219547688U;
+                static const AkUniqueID NEUTRAL = 670611050U;
+                static const AkUniqueID POSITIVE = 1192865152U;
+            } // namespace SWITCH
+        } // namespace MOOD
+
+        namespace RIVER
+        {
+            static const AkUniqueID GROUP = 3605787649U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FAST = 2965380179U;
+                static const AkUniqueID MEDIUM = 2849147824U;
+                static const AkUniqueID SLOW = 787604482U;
+                static const AkUniqueID STOPPED = 2904797076U;
+            } // namespace SWITCH
+        } // namespace RIVER
+
+    } // namespace SWITCHES
 
     namespace GAME_PARAMETERS
     {
