5b3c5b961e6eea2660a2d5ceefc289cc9c07111d
AliCedroni66 <alicedroni66@gmail.com>
2016-05-29 15:03:56 -0500

    Epilogues
    implemented ending screens. Placeholders instead of real endings for now

---
+++ Assets/Scripts/EndingTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EndingTrigger : MonoBehaviour {
+
+	public GameObject myEndingPanel;
+	public Sprite myEpilogueImage; 
+
+	private GameObject m_Panel;
+	// Use this for initialization
+	void Awake()
+	{
+		m_Panel = myEndingPanel;
+		GetComponent<MeshRenderer> ().enabled = false;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Time.timeScale = 0;
+		m_Panel.GetComponent<Image> ().sprite = myEpilogueImage;
+		Color m_color = m_Panel.GetComponent<Image> ().color;
+		m_color.a = 1;
+		m_Panel.GetComponent<Image> ().color = m_color;
+
+	}
+}
